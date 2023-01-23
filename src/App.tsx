c2c4a3e484f9ff2d80dd3835a5e73258d87eef85
Natali Shtofmakher <natalilg2010@gmail.com>
2023-01-23 17:32:52 +0200

    cw#35

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Input } from './components/Input';
 
-import { Timer } from './components/Timer';
 
 function App() {
-  return (
-    <div >
-      <div style={{ display: "flex", alignItems: "center", justifyContent:"space-around", margin: "3em 0 3em"}}>
-        <Timer cityOrCountry="United States"></Timer>
-        <Timer cityOrCountry="Mexico"></Timer>
-      </div>
-      <div style={{ display: "flex", alignItems: "center", justifyContent:"space-around" }}>
-        <Timer cityOrCountry="Canada"></Timer>
-        <Timer cityOrCountry="Costa Rica"></Timer>
-      </div>
-    </div>
+  const properties: React.CSSProperties = {
+    display: 'flex',
+    flexWrap: "wrap",
+  }
+  const [colors, setColors] = React.useState<string[]>([])
+  function creatingDivs(value: string): string {
+    const colors: string[] = value.split("#")
+    setColors(colors.slice());
 
-  );
+    return ''
+  }
+
+  function getDivs(arrColors: string[]):JSX.Element[]{
+    return arrColors.map(color => <div style={{width: "3vw", height:"3vw", backgroundColor: color}}></div>)
+  }
+
+  return <section style={{ display: "flex", flexDirection: "column" }}>
+    <Input placeHolder={'enter colors separeted by #'} inputProcess={creatingDivs} ></Input>
+    <section style={properties}>
+      {getDivs(colors)}
+    </section>
+  </section >
 }
 
 export default App;
