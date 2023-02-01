08491f65cc63acf9f86187c64bdf9843bca4fc97
Natali Shtofmakher <natalilg2010@gmail.com>
2023-02-01 18:21:00 +0200

    hw#37

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { Input } from './components/Input';
-
+import { Life } from './components/Life';
+import lifeConfig from './config/life-game.json'
 
 function App() {
-  const properties: React.CSSProperties = {
-    display: 'flex',
-    flexWrap: "wrap",
-  }
-  const [colors, setColors] = React.useState<string[]>([])
-  function creatingDivs(value: string): string {
-    const colors: string[] = value.split("#")
-    setColors(colors.slice());
 
-    return ''
-  }
 
-  function getDivs(arrColors: string[]):JSX.Element[]{
-    return arrColors.map(color => <div style={{width: "3vw", height:"3vw", backgroundColor: color}}></div>)
-  }
+  return <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <Life dimensions={lifeConfig.dimension} ticInterval={lifeConfig.ticInterval}></Life>
+  </div>
 
-  return <section style={{ display: "flex", flexDirection: "column" }}>
-    <Input placeHolder={'enter colors separeted by #'} inputProcess={creatingDivs} ></Input>
-    <section style={properties}>
-      {getDivs(colors)}
-    </section>
-  </section >
 }
 
 export default App;
